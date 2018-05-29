f4cc1bf80c4bbdda62b4e1c3fc17a283e0ef761f
nicocamarao <nicocamarao@gmail.com>
2018-05-29 14:51:22 -0300

    Camion Main_Contenedores_Camiones y MiContenedor

--- TSIG/src/MiContenedor.java
+++ TSIG/src/MiContenedor.java
@@ -1,4 +1,7 @@
+import java.net.URL;
 import java.util.concurrent.ThreadLocalRandom;
+
+import org.json.simple.JSONObject;
 
 public class MiContenedor implements Runnable {
 
@@ -21,12 +24,15 @@ public class MiContenedor implements Runnable {
 					if (nuevo >= 100) {
 						contenedor[4] = "LLENO";
 						contenedor[2]= "100";
+						updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
 					} else {
 						contenedor[2] = String.valueOf(nuevo);
+						updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
 					}
 				} else if (contenedor[4].equals("CAMION")) {
 					contenedor[2] = "0";
 					contenedor[4] = "DISPONIBLE";
+					updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
 				}
 				double temperatura = ThreadLocalRandom.current().nextDouble(21, 34);
 				if(temperatura > 33.900)
@@ -39,6 +45,20 @@ public class MiContenedor implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
+		
 
+	}
+	
+	public static void updateCapacidadEnGOST(Double capacidad)
+	{
+//		String u = "http://" + gost + ":9080/v1.0/Observations(" + id + ")/";
+//		URL url = new URL(u);
+//		JSONObject body = new JSONObject();
+//
+//		body.put("name", "Camión " + id);
+//		body.put("description", "Conteenedor " + id);
+//		body.put("encodingType", "application/vnd.geo+json");
+//		body.put("location", (JSONObject) ubicacion.get("location"));
 	}
 }
