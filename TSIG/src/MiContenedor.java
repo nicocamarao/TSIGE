d8e1620de01b984919ee69b61722633a6eec2f58
daab88 <daab88@gmail.com>
2018-05-29 22:01:56 -0300

    Update MiContenedor.java

--- TSIG/src/MiContenedor.java
+++ TSIG/src/MiContenedor.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -5,34 +11,41 @@ import org.json.simple.JSONObject;
 
 public class MiContenedor implements Runnable {
 
-	public String[] contenedor;
+	private String estado;
+	private double capacidad;
+	private int thing;
+	private int datastream;
+	private String gost = "18.231.190.192";
 
-	public MiContenedor(String[] strings) {
-		contenedor = strings;
+	public MiContenedor(int thing, int datastream) {
+		estado = "DISPONIBLE";
+		this.thing = thing;
+		this.datastream = datastream;
+		capacidad = 0;
 	}
 
 	public void run() {
 
 		try {
-			while (1 == 1) {
-				
-				Thread.sleep(400);
+			while (true) {
+				long randomtime = (long) ThreadLocalRandom.current().nextDouble(1000, 10000);
+				Thread.sleep(randomtime);
 				// capacidad
 				double random = ThreadLocalRandom.current().nextDouble(0, 1);
-				if (contenedor[4].equals("DISPONIBLE")) {
-					double nuevo = Double.valueOf(contenedor[2]) + random;
+				if (estado.equals("DISPONIBLE")) {
+					double nuevo = capacidad + random;
 					if (nuevo >= 100) {
-						contenedor[4] = "LLENO";
-						contenedor[2]= "100";
-						updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
+						estado = "LLENO";
+						capacidad = 100;
+						updateCapacidadEnGOST(capacidad);
 					} else {
-						contenedor[2] = String.valueOf(nuevo);
-						updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
+						capacidad = nuevo;
+						updateCapacidadEnGOST(capacidad);
 					}
-				} else if (contenedor[4].equals("CAMION")) {
-					contenedor[2] = "0";
-					contenedor[4] = "DISPONIBLE";
-					updateCapacidadEnGOST(Double.valueOf(contenedor[2]));
+				} else if (estado.equals("CAMION")) {
+					capacidad = 0;
+					estado = "DISPONIBLE";
+					updateCapacidadEnGOST(capacidad);
 				}
 				double temperatura = ThreadLocalRandom.current().nextDouble(21, 34);
 				if(temperatura > 33.900)
@@ -44,21 +57,51 @@ public class MiContenedor implements Runnable {
 
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		
 
 	}
 	
-	public static void updateCapacidadEnGOST(Double capacidad)
+	public void updateCapacidadEnGOST(Double capacidad) throws IOException
 	{
-//		String u = "http://" + gost + ":9080/v1.0/Observations(" + id + ")/";
-//		URL url = new URL(u);
-//		JSONObject body = new JSONObject();
-//
-//		body.put("name", "Camión " + id);
-//		body.put("description", "Conteenedor " + id);
-//		body.put("encodingType", "application/vnd.geo+json");
-//		body.put("location", (JSONObject) ubicacion.get("location"));
+		String u = "http://" + gost + ":9080/v1.0/Datastreams(" + datastream + ")/Observations/";
+		URL url = new URL(u);
+		JSONObject body = new JSONObject();
+		body.put("capacidad", capacidad);
+		
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection(/*proxy*/);
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		
+		
+		OutputStream wr= conn.getOutputStream();		
+		
+		wr.write(body.toString().getBytes());
+		wr.flush();
+		
+		if (conn.getResponseCode() != 201) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+		String output = "";
+		String jstring = "";
+		while ((output = br.readLine()) != null) {
+			jstring += output;
+		}		
+		System.out.println("CAPACIDAD ENVIADA AL SERVIDOR, RESPUESTA\n"+jstring);
+		conn.disconnect();		
+		
+		
 	}
 }
